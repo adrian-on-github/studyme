7b53161b49bb8894c8eed61328985eddb474ffca
Adrian <adrian.hassan.ef@gmail.com>
2025-06-06 09:07:43 +0200

    Added AnimatedList

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,37 @@
 import { GridBackground } from "@/components/GridBackgroundWrapper";
 import LoginButton from "@/components/LoginButton";
 import { GraduationCap } from "lucide-react";
+import { AnimatedListDemo } from "@/components/AnimatedList";
 
 export default function Home() {
   return (
-    <main className="lg:px-10 h-full flex justify-center items-center">
-      <GridBackground>
-        <div className="flex flex-row justify-center items-center gap-x-1">
-          <span className="text-black text-sm p">made for</span>
-          <GraduationCap />
-          <span className="text-black text-sm p">students</span>
-        </div>
+    <>
+      <div className="absolute z-50 right-5">
+        <AnimatedListDemo />
+      </div>
 
-        <div className="flex flex-row justify-center items-center">
-          <h1 className="h1 text-4xl font-bold lg:text-8xl text-black">
-            Studying for{" "}
-            <span className="bg-gradient-to-b from-[#7CF7FF] to-[#4B73FF] bg-clip-text text-transparent">
-              everyone
-            </span>
-          </h1>
-        </div>
-        <div className="flex flex-col justify-center items-center my-8 w-full">
-          <LoginButton />
-          <p className="p text-sm/6 mt-1">Start with a 7-day Free Trial</p>
-        </div>
-      </GridBackground>
-    </main>
+      <main className="lg:px-10 h-full flex justify-center items-center">
+        <GridBackground>
+          <div className="flex flex-row justify-center items-center gap-x-1">
+            <span className="text-black text-sm p">made for</span>
+            <GraduationCap />
+            <span className="text-black text-sm p">students</span>
+          </div>
+
+          <div className="flex flex-row justify-center items-center">
+            <h1 className="h1 text-4xl font-bold lg:text-8xl text-black">
+              Studying for{" "}
+              <span className="bg-gradient-to-b from-[#7CF7FF] to-[#4B73FF] bg-clip-text text-transparent">
+                everyone
+              </span>
+            </h1>
+          </div>
+          <div className="flex flex-col justify-center items-center my-8 w-full">
+            <LoginButton />
+            <p className="p text-sm/6 mt-1">Start with a 7-day Free Trial</p>
+          </div>
+        </GridBackground>
+      </main>
+    </>
   );
 }
---
+++ components/AnimatedList.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { AnimatedList } from "@/components/magicui/animated-list";
+
+interface Item {
+  name: string;
+  description: string;
+  icon: string;
+  color: string;
+  time: string;
+}
+
+let notifications = [
+  {
+    name: "Biology Exam TOMORROW?!",
+    description: "Jonas",
+    time: "15m ago",
+
+    icon: "🧬",
+    color: "#fe9a00",
+  },
+  {
+    name: "Math Exam today...",
+    description: "Lena",
+    time: "12m ago",
+    icon: "📚",
+    color: "#7C5DFA",
+  },
+  {
+    name: "Tech Internship Reminder!",
+    description: "Daniel",
+    time: "7m ago",
+    icon: "🧑‍💼",
+    color: "#36CFC9",
+  },
+];
+
+notifications = Array.from({ length: 300 }, () => notifications).flat();
+
+const Notification = ({ name, description, icon, color, time }: Item) => {
+  return (
+    <figure
+      className={cn(
+        "relative mx-auto min-h-fit w-full max-w-[400px] cursor-pointer overflow-hidden rounded-2xl p-4",
+        "transition-all duration-200 ease-in-out hover:scale-[103%]",
+        "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
+        "transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]"
+      )}
+    >
+      <div className="flex flex-row items-center gap-3">
+        <div
+          className="flex size-10 items-center justify-center rounded-2xl"
+          style={{
+            backgroundColor: color,
+          }}
+        >
+          <span className="text-lg">{icon}</span>
+        </div>
+        <div className="flex flex-col overflow-hidden">
+          <figcaption className="flex flex-row items-center whitespace-pre text-lg font-medium dark:text-white">
+            <span className="text-sm sm:text-lg">{name}</span>
+            <span className="mx-1">·</span>
+            <span className="text-xs text-gray-500">{time}</span>
+          </figcaption>
+          <p className="text-sm font-normal dark:text-white/60">
+            {description}
+          </p>
+        </div>
+      </div>
+    </figure>
+  );
+};
+
+export function AnimatedListDemo({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        "relative flex h-[500px] w-full flex-col overflow-hidden p-2",
+        className
+      )}
+    >
+      <AnimatedList>
+        {notifications.map((item, idx) => (
+          <Notification {...item} key={idx} />
+        ))}
+      </AnimatedList>
+
+      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/4 bg-gradient-to-t from-background"></div>
+    </div>
+  );
+}
